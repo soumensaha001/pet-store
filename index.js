cfb974503649e444bf736ba4beb396fcf219692a
Soumen Saha2 <sahasou2@in.ibm.com>
2021-09-08 15:06:17 +0530

    openshift compatible server

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var path = require('path');
 var http = require('http');
 
 var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT||8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 // swaggerRouter configuration
 var options = {
@@ -17,8 +18,9 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
+http.createServer(app).listen(serverPort,server_ip_address , function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
+
 
